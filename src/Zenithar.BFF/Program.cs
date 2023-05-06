149552537bba38b1f39dccd1f6166ccdf514c581
Tripletri <nicketa329@gmail.com>
2023-05-06 16:58:00 +0500

    add image uploading to bucket via aws s3

--- src/Zenithar.BFF/Program.cs
+++ src/Zenithar.BFF/Program.cs
@@ -1,5 +1,11 @@
 using System.Reflection;
+using Amazon;
+using Amazon.Extensions.NETCore.Setup;
+using Amazon.Runtime;
+using Amazon.S3;
+using Microsoft.Extensions.Options;
 using Zenithar.BFF.Clients.Products;
+using Zenithar.BFF.Configuration;
 using Zenithar.BFF.Repositories;
 using Zenithar.BFF.Services;
 using Zenithar.BFF.WebApi.Filters;
@@ -24,6 +30,33 @@ services.AddSingleton<IBasketRepository, InMemoryBasketRepository>();
 services.AddSingleton<IProductsService, ProductsService>();
 services.AddSingleton<HttpClient>();
 
+// AWS
+services.AddOptions<ApplicationAWSOptions>()
+    .BindConfiguration("AWS");
+
+services.AddDefaultAWSOptions(x =>
+{
+    var options = x.GetRequiredService<IOptions<ApplicationAWSOptions>>().Value;
+
+    AWSConfigs.AWSRegion = "ru-central1";
+
+    return new AWSOptions
+    {
+        Credentials = new BasicAWSCredentials(options.AccessKeyId, options.AccessKeySecret)
+    };
+});
+
+services.AddSingleton<IAmazonS3>(x =>
+{
+    var options = x.GetRequiredService<AWSOptions>();
+    var config = new AmazonS3Config
+    {
+        ServiceURL = "https://s3.yandexcloud.net"
+    };
+    
+    return new AmazonS3Client(options.Credentials, config);
+});
+
 // ---------------------
 var app = builder.Build();
 
@@ -43,4 +76,4 @@ app.UseSwaggerUI();
 app.UseRouting();
 app.MapFallbackToFile("index.html");
 
-app.Run();
+await app.RunAsync();
---
+++ src/Zenithar.BFF/Services/ProductsService.cs
@@ -0,0 +1,69 @@
+﻿using Amazon.S3;
+using Microsoft.Extensions.Options;
+using Zenithar.BFF.Clients.Products;
+using Zenithar.BFF.Configuration;
+using Zenithar.BFF.Core;
+using Zenithar.ProductsAPI.WebApi.Dtos;
+
+namespace Zenithar.BFF.Services;
+
+internal sealed class ProductsService : IProductsService
+{
+    private readonly ILogger<ProductsService> logger;
+    private readonly IOptions<ApplicationAWSOptions> options;
+    private readonly IProductsClient client;
+    private readonly IAmazonS3 s3;
+
+    public ProductsService(ILogger<ProductsService> logger, IOptions<ApplicationAWSOptions> options,
+                           IProductsClient client, IAmazonS3 s3)
+    {
+        this.logger = logger;
+        this.options = options;
+        this.client = client;
+        this.s3 = s3;
+    }
+
+    public async Task<Product> Create(string name, int price, Stream image,
+                                      CancellationToken cancellationToken = default)
+    {
+        var previewUrl = await UploadImage(image, cancellationToken);
+
+        var request = new V1CreateProductRequest(name, price, previewUrl);
+
+        logger.LogInformation("Creating product");
+        var product = await client.Create(request, cancellationToken);
+        logger.LogInformation("Successfully created product. '{ProductId}'", product.Id);
+
+        return product;
+    }
+
+    private async Task<string> UploadImage(Stream image, CancellationToken cancellationToken)
+    {
+        var bucketName = options.Value.BucketName;
+        var objectKey = Guid.NewGuid().ToString();
+
+        logger.LogInformation("Uploading image. '{ObjectKey}'", objectKey);
+
+        await s3.UploadObjectFromStreamAsync(bucketName, objectKey, image, new Dictionary<string, object>(),
+            cancellationToken);
+
+        logger.LogInformation("Successfully uploaded image. '{ObjectKey}'", objectKey);
+
+        var previewUrl = $"https://storage.yandexcloud.net/zenithar/{objectKey}";
+        return previewUrl;
+    }
+
+    public async Task<Product> Update(string id, string name, int price, Stream image,
+                                      CancellationToken cancellationToken = default)
+    {
+        var previewUrl = await UploadImage(image, cancellationToken);
+
+        var request = new V1UpdateProductRequest(name, price, previewUrl);
+
+        logger.LogInformation("Updating product. '{ProductId}'", id);
+        var product = await client.Update(id, request, cancellationToken);
+        logger.LogInformation("Successfully updated product. '{ProductId}'", id);
+
+        return product;
+    }
+}
